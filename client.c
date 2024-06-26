23204089861a67bd4000a6c0614c36ebe557a94b
Davidls10 <104114592+Davidls10@users.noreply.github.com>
2024-06-25 22:52:08 -0300

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,4 @@
-#include <arpa/inet.h>
+  GNU nano 6.2                                                                                         client.c                                                                                                  #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -7,34 +7,34 @@
 #define PORT 8080
 
 int main(int argc, char const* argv[]) {
-	int status, valread, client_fd;
-	struct sockaddr_in serv_addr;
-	char *hello = "Hello from client";
-	char buffer[1024] = { 0 };
+        int status, valread, client_fd;
+        struct sockaddr_in serv_addr;
+        char *hello = "Hello from client";
+        char buffer[1024] = { 0 };
 
-	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("\n Socket creation error \n");
-		return -1;
-	}
+        if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                printf("\n Socket creation error \n");
+                return -1;
+        }
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
 
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-		printf("\n Invalid address or address not supported \n");
-		return -1;
-	}
+        if (inet_pton(AF_INET, "192.168.100.221", &serv_addr.sin_addr) <= 0) { // raspberry ip
+                printf("\n Invalid address or address not supported \n");
+                return -1;
+        }
 
-	if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-		printf("\nConnection failed\n");
-		return -1;
-	}
+        if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+                printf("\nConnection failed\n");
+                return -1;
+        }
 
-	send(client_fd, hello, strlen(hello), 0);
-	printf("Hello message sent\n");
-	valread = read(client_fd, buffer, 1024 - 1);
-	printf("%s\n", buffer);
+        send(client_fd, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+        valread = read(client_fd, buffer, 1024 - 1);
+        printf("%s\n", buffer);
 
-	close(client_fd);
-	return 0;
+        close(client_fd);
+        return 0;
 }
